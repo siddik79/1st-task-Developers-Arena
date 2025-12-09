b9b709f226e37bb273020c054e862147223b4edf
siddik79 <shortskeeda498@gmail.com>
2025-12-09 12:14:46 +0530

    Update and rename new.py to personal_intro.py

--- personal_intro.py
+++ personal_intro.py
@@ -2,4 +2,5 @@ name = input("Enter Your Name:")
 age = int(input("Enter Your Age:"))
 hobby = input("Enter Your Hobby:")
 message = f"Wlecome {name} to Dvelopers Arena keep your {hobby} growing {name}"
+
 print(message ) 
